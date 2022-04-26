9cfa715cc40a958f810c8b30f7942122c68ae2a0
joakobarros <joaquinbarroscs@gmail.com>
2022-04-26 18:51:21 -0300

    actualización css

---
+++ assets/js/Script.js
@@ -0,0 +1,30 @@
+var num1 = 1;
+var num2 = 7;
+var num3 = 0;
+
+//comparativa número mayor
+
+if (num1 > num2 && num1 > num3){
+    // el primer número es el mayor
+}else{
+
+    if (num2 > num1 && num2 > num3){ 
+        // el segundo número es el mayor
+    }else{
+        // el tercer número es el mayor  
+    }
+}
+
+
+//comparativa número menor
+
+if (num1 < num2 && num1 < num3){
+    // el primer número es el menor 
+}else{
+    
+    if(num2 < num1 && num2 < num3){
+        //el segundo número es el menor
+    }else{
+        //el tercer número es el menor
+    }
+}
